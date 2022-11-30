aefec5025c68a09dcd41a346d6ccbea37addc4cb
Dania2021 <dania.khanam@gmail.com>
2022-11-30 00:22:18 +0000

    Implement emailjs integration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    emailjs.send("service_e8zu2zi", "rosie", {
+        //keys for this object are all equal to the
+        //parameters names that we've set in our email
+        //template back on EmailJS
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("Success", response);
+        },
+        function(error) {
+            console.log("Failed", error);
+        });
+}  
